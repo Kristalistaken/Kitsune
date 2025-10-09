abff3c4b0861be7d4f340753c060b34573c32962
Marcy <orkinin@gmail.com>
2025-10-09 15:51:04 +0200

    fixed Joker path

--- sillyFoxCoding.lua
+++ sillyFoxCoding.lua
@@ -1,4 +1,4 @@
---Hopefully makes it so stuff can be read
+--Hopefully makes it so stuff can be read (Does quite literally nothing atm I'm pretty sure)
 local current_mod = SMODS.current_mod
 local mod_path = SMODS.current_mod.path
 local config = SMODS.current_mod.config
@@ -12,7 +12,7 @@ local joker_list = {
 
 
 }
-assert(SMODS.load_file("indiv-jokers/Kitsune.lua"))()
+assert(SMODS.load_file("idv-jokers/Kitsune.lua"))()
 SMODS.Atlas({
     key = "fox_jokers",
     atlas = "fox_atlas",
