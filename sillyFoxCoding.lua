f8473e809b0a79ccdfe66a9a7d8a04ad3efd35a3
Marcy <orkinin@gmail.com>
2025-10-11 01:14:29 +0200

    Forgot to add file access

--- sillyFoxCoding.lua
+++ sillyFoxCoding.lua
@@ -1,4 +1,5 @@
 assert(SMODS.load_file("idv-jokers/Kitsune.lua"))()
+assert(SMODS.load_file("idv-jokers/Balanced_Joker.lua"))()
 SMODS.Atlas({
     key = "fox_jokers",
     atlas_table = "ASSET_ATLAS",
