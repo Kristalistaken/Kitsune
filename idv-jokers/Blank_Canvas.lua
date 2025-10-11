46f3484ef05e04ac98bd2ffd8a1963f34c296dd3
izniko <052sico250@gmail.com>
2025-10-11 18:08:02 +0100

    Blank Canvas added

--- idv-jokers/Blank_Canvas.lua
+++ idv-jokers/Blank_Canvas.lua
@@ -1,26 +1,49 @@
--- Blank Card
--- Editions have double the effect on this Joker
--- (will always have an edition)
-
 SMODS.Joker {
 	key = 'blankcanvas',
 	atlas = 'fox_placeholder',
 	rarity = 2,
 	pos = { x = 0, y = 0 },
-	cost = 4,
-	blueprint_compat = false,
+	cost = 6,
+	blueprint_compat = true,
 	perishable_compat = true,
-	config = { extra = { xmultvar = 1, ninesplayed = 0, odds = 2 } },
---	set_ability = function(self, card, initial)
---        card:set_edition("e_polychrome", true)
-
---	end,
-    loc_vars = function(self, info_queue, card)       
-      --  local new_numerator, new_denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'j_fox_kitsune') 
-      --  return {vars = {card.ability.extra.xmultvar, card.ability.extra.ninesplayed, new_numerator, new_denominator}}
-    
-    end,
-
-
-
+	config = { extra = { chips = 0, mult = 0, xmult = 1, jokerslots = 0 } },
+    add_to_deck = function(self, card, from_debuff)
+		if not from_debuff then
+			local edition = poll_edition('fox_blankcanvas', nil, false, true, {'e_foil', 4, 'e_holo', 3, 'e_polychrome', 2, 'e_negative', 1})
+        	card:set_edition(edition, true)
+		end
+	end,
+	loc_vars = function(self, info_queue, card)
+		if card.edition and card.edition.base then
+		elseif card.edition and card.edition.e_foil then
+			card.ability.extra.chips = 50
+		elseif card.edition and card.edition.e_holo then
+			card.ability.extra.mult = 10
+		elseif card.edition and card.edition.e_polychrome then
+			card.ability.extra.xmult = 1.5
+		elseif card.edition and card.edition.e_negative then
+			G.jokers.config.card_limit = G.jokers.config.card_limit + 1
+			card.ability.extra.jokerslots = 1
+			G.was_negative = false
+		end
+		return { vars = {
+			card.ability.extra.chips,
+			card.ability.extra.mult,
+			card.ability.extra.xmult,
+			card.ability.extra.jokerslots
+		} }
+	end,
+	calculate = function(self, card, context)
+		if context.selling_card == true and G.was_negative == true and not context.blueprint then
+			G.jokers.config.card_limit = G.jokers.config.card_limit - 1
+		end
+		if context.cardarea == G.jokers and context.joker_main then
+			return {
+				chips = card.ability.extra.chips,
+				mult = card.ability.extra.mult,
+				xmult = card.ability.extra.xmult,
+				jokerslots = card.ability.extra.jokerslots
+			}
+		end
+	end
 }
--- localization/default.lua
+++ localization/default.lua
@@ -4,16 +4,26 @@ return {
             j_fox_kitsune = {
                 name = "Kitsune",
                 text = {
-                    'This Joker has a {C:green}#3# in #4#{} chance to gain',
-					'{X:red,C:white}X0.5{} Mult every {C:attention}9{} {C:inactive}[#2#]{} 9s played',
-                    '{C:inactive}(Currently{} {X:red,C:white}X#1#{} {C:inactive}Mult){}'
+--                    'Has a {C:green}#3# in #4#{} chance to gain',
+					'Gains {X:red,C:white}X0.5{} mult every {C:attention}9{} {C:inactive}[#2#]{} {C:attention}9{}s played',
+                    '{C:inactive}(Currently{} {X:red,C:white}X#1#{} {C:inactive}mult){}'
         }
             },
 
             j_fox_balancedjoker = {
                 name = "Balanced Joker",
                 text = {
-                    '{C:green}#1# in #2#{} chance for {C:red}+3600{} Mult'
+                    'Has a {C:green}#1# in #2#{} chance for {C:red}+3600{} mult',
+                    '{C:inactive}(Always is eternal and {C:dark_edition}Negative{}{C:inactive}){}'
+        }
+            },
+            j_fox_blankcanvas = {
+                name = "Blank Canvas",
+                text = {
+                    'Copies the effect of its edition,',
+                    'always has an edition',
+                    '{C:inactive}(Currently {}{C:blue}+#1#{}{C:inactive} chips, {}{C:red}+#2#{}{C:inactive} mult,{}',
+                    '{X:red,C:white}X#3#{}{C:inactive} mult, {}{C:dark_edition}+#4#{}{C:inactive} Joker slots){}'
         }
             },
 
