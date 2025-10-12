8e1b3b7c15cb720e4d624027ca04c1735360ed52
izniko <052sico250@gmail.com>
2025-10-12 04:08:33 +0100

    Added 3 new Jokers and misc changes added

--- localization/default.lua
+++ localization/default.lua
@@ -1,33 +1,56 @@
 return {
-    descriptions = {      
+    descriptions = {
         Joker = {
             j_fox_kitsune = {
                 name = "Kitsune",
                 text = {
 --                    'Has a {C:green}#3# in #4#{} chance to gain',
-					'Gains {X:red,C:white}X0.5{} mult every {C:attention}9{} {C:inactive}[#2#]{} {C:attention}9{}s played',
+					'This Joker gains {X:red,C:white}X0.5{} mult',
+                    'every {C:attention}9{} {C:inactive}[#2#]{} {C:attention}9{}s played',
                     '{C:inactive}(Currently{} {X:red,C:white}X#1#{} {C:inactive}mult){}'
         }
             },
-
             j_fox_balancedjoker = {
                 name = "Balanced Joker",
                 text = {
                     'Has a {C:green}#1# in #2#{} chance for {C:red}+3600{} mult',
-                    '{C:inactive}(Always is eternal and {C:dark_edition}Negative{}{C:inactive}){}'
+                    '{C:inactive}(Always is Eternal and {C:dark_edition}Negative{}{C:inactive}){}'
         }
             },
             j_fox_blankcanvas = {
                 name = "Blank Canvas",
                 text = {
-                    'Copies the effect of its edition,',
-                    'always has an edition',
+                    'Copies the effect of editions it',
+                    'has or had, always has an edition',
                     '{C:inactive}(Currently {}{C:blue}+#1#{}{C:inactive} chips, {}{C:red}+#2#{}{C:inactive} mult,{}',
                     '{X:red,C:white}X#3#{}{C:inactive} mult, {}{C:dark_edition}+#4#{}{C:inactive} Joker slots){}'
         }
             },
-
-            
+            j_fox_armed = {
+                name = "Armed and Dangerous",
+                text = {
+                    'Each played card gives {X:red,C:white}X1{} mult',
+                    'when scored, this is increased',
+                    'by {X:red,C:white}X0.5{} mult for each scored card,',
+                    'resets after the hand is scored'
+        }
+            },
+            j_fox_loan = {
+                name = "Loan Shark",
+                text = {
+                    'Gain {C:money}$50{} on pickup,',
+                    'lose {C:money}$100{} when sold'
+        }
+            },
+            j_fox_singing = {
+                name = "Singing Machine",
+                text = {
+                    'Has a {C:green}#1# in #2#{} chance to',
+                    '{C:attention}destroy{} all other {C:attention}Jokers{} and to',
+                    'permanently copy their abilities',
+                    'after beating a {C:attention}Boss Blind{}'
+        }
+            }
         },
     },
 }
