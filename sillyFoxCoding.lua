56f2cf526f8f09ebfe8e1b0e7825b248270c9b88
izniko <052sico250@gmail.com>
2025-10-12 11:12:14 +0100

    Commented out Singing Machine as it is not working

--- sillyFoxCoding.lua
+++ sillyFoxCoding.lua
@@ -1,7 +1,7 @@
 assert(SMODS.load_file("idv-jokers/Kitsune.lua"))()
 assert(SMODS.load_file("idv-jokers/Balanced_Joker.lua"))()
 assert(SMODS.load_file("idv-jokers/Blank_Canvas.lua"))()
-assert(SMODS.load_file("idv-jokers/Singing_Machine.lua"))()
+--assert(SMODS.load_file("idv-jokers/Singing_Machine.lua"))()
 assert(SMODS.load_file("idv-jokers/Armed_and_Dangerous.lua"))()
 assert(SMODS.load_file("idv-jokers/Loan_Shark.lua"))()
 SMODS.Atlas({
