ad3c61480141b66b73a0a49336c92c30b708b549
izniko <052sico250@gmail.com>
2025-10-19 18:28:07 +0100

    Changed Armed and Dangerous a bit

--- idv-jokers/Armed_and_Dangerous.lua
+++ idv-jokers/Armed_and_Dangerous.lua
@@ -11,6 +11,7 @@ SMODS.Sound({
 })
 
 G.playsound = true
+G.playsound2 = false
 
 local joker = {
 	key = 'armed',
@@ -23,18 +24,16 @@ local joker = {
     loc_vars = function(self, info_queue, card)
         return {vars = {card.ability.extra.xmultvar}}
     end,
-
     calculate = function(self, card, context)
         if context.cardarea == G.play and context.individual then
-            if G.playsound == true then
-                G.playsound = false
-                return { message = "ARMED AND DANGEROUS", sound = 'fox_armed', pitch = 1, volume = 1.5 }
-            end
             if card.ability.extra.retriggeredcard and context.other_card == card.ability.extra.retriggeredcard then
-                card.ability.extra.xmultvar = (card.ability.extra.xmultvar) + 0.1
-            if card.ability.extra.retriggeredcard and context.other_card == card.ability.extra.retriggeredcard then
-                return { message = "AGAIN", sound = 'fox_again', pitch = 1, volume = 2, extra = { Xmult = card.ability.extra.xmultvar } }
-            end
+                card.ability.extra.xmultvar = card.ability.extra.xmultvar + 0.1
+                if G.playsound == true then
+                    G.playsound = false
+                    return { Xmult = card.ability.extra.xmultvar, message = "ARMED AND DANGEROUS", sound = 'fox_armed', pitch = 1, volume = 1.5 }
+                else
+                    return { Xmult = card.ability.extra.xmultvar, message = "AGAIN", sound = 'fox_again', pitch = 1, volume = 2 }
+                end
             else
                 if not context.blueprint then
                     card.ability.extra.retriggeredcard = context.other_card
